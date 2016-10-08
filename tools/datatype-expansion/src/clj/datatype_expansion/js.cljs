404d9fed0f2d67671218dd2b232cf67f596d9f39
Antonio Garrote <antoniogarrote@gmail.com>
2016-10-08 16:08:35 +0100

    Fixes #37. Providing synch interface

--- tools/datatype-expansion/src/clj/datatype_expansion/js.cljs
+++ tools/datatype-expansion/src/clj/datatype_expansion/js.cljs
@@ -8,26 +8,32 @@
 (enable-console-print!)
 
 (defn ^:export expandedForm [type-form typing-context cb]
-  (go (try
-        (let [type-form (-> type-form js->clj keywordize-keys)
-              typing-context (-> typing-context
-                                 js->clj
-                                 (->> (map (fn [[k v]] [k (keywordize-keys v)]))
-                                      (into {})))
-              result (expanded/expanded-form (js->clj type-form) (js->clj typing-context))]
-          (cb nil (clj->js result)))
-        (catch js/Error error
-          (cb error nil))))
-  nil)
+  (try
+    (let [type-form (-> type-form js->clj keywordize-keys)
+          typing-context (-> typing-context
+                             js->clj
+                             (->> (map (fn [[k v]] [k (keywordize-keys v)]))
+                                  (into {})))
+          result (expanded/expanded-form (js->clj type-form) (js->clj typing-context))
+          result (clj->js result)]
+      (when (some? cb) (cb nil result))
+      result)
+    (catch js/Error error
+      (if (some? cb)
+        (cb error nil)
+        (throw error)))))
 
 (defn ^:export canonicalForm [expanded-form cb]
-  (go (try
-        (let [expanded-form (-> expanded-form js->clj keywordize-keys)
-              result (canonical/canonical-form expanded-form)]
-          (cb nil (clj->js result)))
-        (catch js/Error error
-          (cb error nil))))
-  nil)
+  (try
+    (let [expanded-form (-> expanded-form js->clj keywordize-keys)
+          result (canonical/canonical-form expanded-form)
+          result (clj->js result)]
+      (when (some? cb) (cb nil result))
+      result)
+    (catch js/Error error
+      (if (some? cb)
+        (cb error nil)
+        (throw error)))))
 
 (defn -registerInterface [] nil)
 
