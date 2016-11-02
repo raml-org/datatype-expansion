62e56a0bf9d2c7eef71ebdbc8e1105c19303628c
Antonio Garrote <antoniogarrote@gmail.com>
2016-11-02 12:29:52 +0000

    version 0.15 bump

--- tools/datatype-expansion/project.clj
+++ tools/datatype-expansion/project.clj
@@ -1,4 +1,4 @@
-(defproject datatype-expansion "0.0.14"
+(defproject datatype-expansion "0.0.15"
   :description "Utility to expand RAML Data Types into a single canonical form"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/"}
