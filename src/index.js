53c92d192035f1004bfeb4f92fbc5bf6de0b6a83
Artem Kostiuk <postatum@gmail.com>
2017-06-22 10:58:46 +0300

    Clean index.js

--- src/index.js
+++ src/index.js
@@ -5,14 +5,3 @@ module.exports = {
   expandedForm: expanded.expandedForm,
   canonicalForm: canonical.canonicalForm
 }
-
-// TEST:
-//
-// var types = require('../test/fixtures/types')
-// var type = types['Song']
-// expanded.expandedForm(type, null, function(err, expForm) {
-//   console.log('Expanded >>', expForm)
-//   canonical.canonicalForm(expForm, function(err, canForm) {
-//     console.log('Canonical >>', canForm)
-//   })
-// })
