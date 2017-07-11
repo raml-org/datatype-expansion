4275b74c0514abd769437c2b5b9d856abcf48c63
Artem Kostiuk <postatum@gmail.com>
2017-07-11 08:25:49 +0300

    Trying to fix fixtures

--- test/canonical.js
+++ test/canonical.js
@@ -5,16 +5,15 @@
 const _ = require('lodash')
 const expect = require('chai').expect
 const types = require('./fixtures/types').types
-const forms = require('./fixtures/types').canonical
+const forms = require('./fixtures/canonical_forms')
 const expandedForm = require('..').expandedForm
 const canonicalForm = require('..').canonicalForm
 
 describe('canonicalForm()', function () {
   _.each(types, function (type, name) {
-    if (name === 'canonical' || name === 'expanded' ||
-        // TODO: fix tests
-        name === 'WithInheritance' || name === 'ValidConstraintsInheritance') {
-      return
+    // These two types give error in cljs library
+    if (name === 'WithInheritance' || name === 'ValidConstraintsInheritance') {
+        return
     }
     it('should generate canonical form of type ' + name, function () {
       expandedForm(types[name], types, function (err, expForm) {
--- test/expanded.js
+++ test/expanded.js
@@ -5,12 +5,15 @@
 const _ = require('lodash')
 const expect = require('chai').expect
 const types = require('./fixtures/types').types
-const forms = require('./fixtures/types').expanded
+const forms = require('./fixtures/expanded_forms')
 const expandedForm = require('..').expandedForm
 
 describe('expandedForm()', function () {
   _.each(types, function (type, name) {
-    if (name === 'canonical' || name === 'expanded') return
+    // These two types give error in cljs library
+    if (name === 'WithInheritance' || name === 'ValidConstraintsInheritance') {
+        return
+    }
     it('should generate expanded form of type ' + name, function () {
       expandedForm(types[name], types, function (err, expForm) {
         expect(err).to.equal(null)
