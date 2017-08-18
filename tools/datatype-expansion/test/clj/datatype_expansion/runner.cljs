758226184e1c81d1c3db83f59a1da262b3758cbe
Jonathan Stoikovitch <jstoiko@users.noreply.github.com>
2017-08-18 14:38:12 -0700

    removing old 'tool/' folder

--- tools/datatype-expansion/test/clj/datatype_expansion/runner.cljs
+++
@@ -1,9 +0,0 @@
-(ns datatype-expansion.runner
-  (:require [doo.runner :refer-macros [doo-tests]]
-            [datatype-expansion.expanded-form-test]
-            [datatype-expansion.canonical-form-test]
-            [datatype-expansion.integration-test]))
-
-(doo-tests 'datatype-expansion.expanded-form-test
-           'datatype-expansion.canonical-form-test
-           'datatype-expansion.integration-test)
