026e66d876d32cbc0c6e8ca40b181aaa03baa079
Jonathan Stoikovitch <jstoiko@users.noreply.github.com>
2018-10-31 11:46:00 -0700

    update babel/rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,24 @@
 import babel from 'rollup-plugin-babel'
-import uglify from 'rollup-plugin-uglify'
+import { uglify } from 'rollup-plugin-uglify'
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
 
 export default {
-  strict: true,
-  sourcemap: true,
   input: 'src/index.js',
+  output: {
+    strict: true,
+    sourcemap: true
+  },
   plugins: [
+    babel({
+      sourceMap: true,
+      exclude: 'node_modules/**'
+    }),
     nodeResolve({
       jsnext: true,
       main: true
     }),
     commonjs(),
-    babel({
-      sourceMap: true,
-      exclude: 'node_modules/**'
-    }),
     uglify()
   ]
 }
